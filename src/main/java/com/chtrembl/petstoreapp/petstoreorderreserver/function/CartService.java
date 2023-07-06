a98ec13f5dcd1b7ed4d16fe3eb01bc371a914c31
Egor Piankov <piankovegor@gmail.com>
2023-07-06 16:30:04 +0200

    Moving update method to other file

--- src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/function/CartService.java
+++ src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/function/CartService.java
@@ -1,23 +1,18 @@
-package com.chtrembl.petstoreapp.petstoreorderreserver;
+package com.chtrembl.petstoreapp.petstoreorderreserver.function;
 
 import com.microsoft.azure.functions.ExecutionContext;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.function.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 
 import java.util.Map;
 import java.util.function.Function;
 
-@SpringBootApplication
-public class ApplicationConfig {
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationConfig.class, args);
-	}
-
+@Configuration
+public class CartService {
 	@Bean
-	public Function<Message<String>, String> uppercase(JsonMapper mapper) {
+	public Function<Message<String>, String> update(JsonMapper mapper) {
 		return message -> {
 			String value = message.getPayload();
 			ExecutionContext context = (ExecutionContext) message.getHeaders().get("executionContext");
@@ -25,11 +20,12 @@ public class ApplicationConfig {
 				Map<String, String> map = mapper.fromJson(value, Map.class);
 
 				if (map != null)
-					map.forEach((k, v) -> map.put(k, v != null ? v.toUpperCase() : null));
+					map.forEach((k, v) -> map.put(k, v != null ? v.toLowerCase() : null));
 
 				if (context != null)
 					context.getLogger().info(new StringBuilder().append("Function: ")
-							.append(context.getFunctionName()).append(" is uppercasing ").append(value.toString()).toString());
+							.append(context.getFunctionName())
+							.append(" is uppercasing ").append(value.toString()).toString());
 
 				return mapper.toString(map);
 			} catch (Exception e) {
