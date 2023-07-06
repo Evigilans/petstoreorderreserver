b52575a1c52972024c122da5d86740cdfed4b30a
Egor Piankov <piankovegor@gmail.com>
2023-07-06 02:48:26 +0200

    Adding sout for debug purposes

--- src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/controller/OrderReserverHandler.java
+++ src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/controller/OrderReserverHandler.java
@@ -8,17 +8,20 @@ import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
 
 import java.util.Optional;
 
-public class OrderItemsReserverController extends AzureSpringBootRequestHandler<Object, ResponseDTO> {
+@Log4j2
+public class OrderReserverHandler extends AzureSpringBootRequestHandler<Object, ResponseDTO> {
+
 	@FunctionName("updateCart")
 	public ResponseDTO update(
-			@HttpTrigger(name = "updateCartRequest", methods = {HttpMethod.POST},
-					authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<Cart>> request,
+			@HttpTrigger(name = "updateCartRequest", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<Cart>> request,
 			ExecutionContext context) {
 		System.out.println("OrderItemsReserverController.update");
-		return handleRequest(request.getBody(), context);
+		log.error("OrderReserverHandler");
+		return handleRequest(request.getBody().get(), context);
 	}
 }
--- src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/entity/Cart.java
+++ src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/entity/Cart.java
@@ -1,4 +1,14 @@
 package com.chtrembl.petstoreapp.petstoreorderreserver.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Cart {
+	private Integer quantity;
 }
