eae12367a978ac3f20bb923c28856c6dac379c71
Egor Piankov <piankovegor@gmail.com>
2023-07-06 18:08:42 +0200

    Fixing 500 error

--- src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/service/CartService.java
+++ src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/service/CartService.java
@@ -3,7 +3,6 @@ package com.chtrembl.petstoreapp.petstoreorderreserver.service;
 import com.azure.core.util.BinaryData;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -11,23 +10,16 @@ import java.util.Map;
 @Service
 public class CartService {
 
-	@Value("${azure.storage.connection-string}")
-	private String connectionString;
-
-	@Value("${azure.storage.container-name}")
-	private String containerName;
-
 	public void update(Map<String, String> map) {
 		String fileName = java.util.UUID.randomUUID().toString();
 		BlobClient blobClient = new BlobClientBuilder()
-				.connectionString(connectionString)
-				.containerName(containerName)
+				.connectionString(System.getenv("CONNECTION_STRING"))
+				.containerName("msajdsa")
 				.blobName(fileName)
 				.buildClient();
 
 		if (map != null) {
 			map.forEach((k, v) -> map.put(k, v != null ? v.toUpperCase() : null));
-			map.put("connectionString", connectionString);
 			blobClient.upload(BinaryData.fromObject(map));
 		}
 	}
