fd04b66d0ac4ce825b6cf948748f9041a5067d4b
Egor Piankov <piankovegor@gmail.com>
2023-07-06 17:39:30 +0200

    Adding saving file to Storage Account

--- src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/service/CartService.java
+++ src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/service/CartService.java
@@ -1,5 +1,8 @@
 package com.chtrembl.petstoreapp.petstoreorderreserver.service;
 
+import com.azure.core.util.BinaryData;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +14,21 @@ public class CartService {
 	@Value("${azure.storage.connection-string}")
 	private String connectionString;
 
+	@Value("${azure.storage.container-name}")
+	private String containerName;
+
 	public void update(Map<String, String> map) {
+		String fileName = java.util.UUID.randomUUID().toString();
+		BlobClient blobClient = new BlobClientBuilder()
+				.connectionString(connectionString)
+				.containerName(containerName)
+				.blobName(fileName)
+				.buildClient();
+
 		if (map != null) {
 			map.forEach((k, v) -> map.put(k, v != null ? v.toUpperCase() : null));
 			map.put("connectionString", connectionString);
+			blobClient.upload(BinaryData.fromObject(map));
 		}
 	}
 }
