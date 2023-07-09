981ac8b4e56b6134a3d56f775c5072dbd0e8afeb
Egor Piankov <piankovegor@gmail.com>
2023-07-09 17:33:36 +0200

    Fixing error

--- src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/ApplicationConfig.java
+++ src/main/java/com/chtrembl/petstoreapp/petstoreorderreserver/ApplicationConfig.java
@@ -1,11 +1,44 @@
 package com.chtrembl.petstoreapp.petstoreorderreserver;
 
+import com.microsoft.azure.functions.ExecutionContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.json.JsonMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+
+import java.util.Map;
+import java.util.function.Function;
 
 @SpringBootApplication
 public class ApplicationConfig {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationConfig.class, args);
+	}
+
+	@Bean
+	public Function<Message<String>, String> uppercase(JsonMapper mapper) {
+		return message -> {
+			String value = message.getPayload();
+			ExecutionContext context = (ExecutionContext) message.getHeaders().get("executionContext");
+			try {
+				Map<String, String> map = mapper.fromJson(value, Map.class);
+
+				if (map != null)
+					map.forEach((k, v) -> map.put(k, v != null ? v.toUpperCase() : null));
+
+				if (context != null)
+					context.getLogger().info(new StringBuilder().append("Function: ")
+							.append(context.getFunctionName()).append(" is uppercasing ").append(value.toString()).toString());
+
+				return mapper.toString(map);
+			} catch (Exception e) {
+				e.printStackTrace();
+				if (context != null)
+					context.getLogger().severe("Function could not parse incoming request");
+
+				return ("Function error: - bad request");
+			}
+		};
 	}
 }
